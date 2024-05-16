6fdf1e42f0ef0636d7e6d82d903d1be4dc83d98b
perompak_kode <iqbaltech29@gmail.com>
2024-05-16 13:28:53 +0700

    Initial commit

---
+++ app/src/main/java/com/jimbonlemu/clefer/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jimbonlemu.clefer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jimbonlemu.clefer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+
+        }
+    }
+}

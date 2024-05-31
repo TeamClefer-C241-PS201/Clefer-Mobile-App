81198dfc41eef8d437dc06fe9a57db86355bc405
perompak_kode <iqbaltech29@gmail.com>
2024-06-01 03:41:37 +0700

    feat(register): update register page layout and functionality
    - Updated the layout XML for the register page
      - Removed ImageButton for back navigation
      - Added TextView for main headline with new string resource
      - Adjusted layout margins and constraints for better alignment
      - Updated field hint texts and labels with new string resources
      - Changed button text to "Daftar"
      - Applied style adjustments to align with new design mockup
    - Refactored RegisterActivity
      - Added click listener for "Masuk Sekarang" TextView to navigate to SignInActivity
      - Included intent with flags to clear task and start new task for navigation
    - Updated string resources
      - Added new string resources for register page
      - Adjusted existing string resources for consistency

--- app/src/main/java/com/jimbonlemu/clefer/views/RegisterActivity.kt
+++ app/src/main/java/com/jimbonlemu/clefer/views/RegisterActivity.kt
@@ -1,22 +1,29 @@
 package com.jimbonlemu.clefer.views
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.jimbonlemu.clefer.R
 import com.jimbonlemu.clefer.core.CoreActivity
 import com.jimbonlemu.clefer.databinding.ActivityRegisterBinding
 
 class RegisterActivity : CoreActivity<ActivityRegisterBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.apply {
+            tvBtnSignIn.setOnClickListener {
+                intentSignIn()
+            }
+        }
     }
 
     override fun setupBinding(layoutInflater: LayoutInflater): ActivityRegisterBinding =
         ActivityRegisterBinding.inflate(layoutInflater)
 
+    private fun intentSignIn() {
+        startActivity(Intent(this@RegisterActivity, SignInActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        })
+        finish()
+    }
 
 }

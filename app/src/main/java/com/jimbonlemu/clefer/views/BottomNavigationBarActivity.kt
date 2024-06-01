dc3ededd6ae155cd2294d8ea9cd279b96837ebcb
perompak_kode <iqbaltech29@gmail.com>
2024-06-02 05:07:59 +0700

    feat: enhance BottomNavigationBarActivity layout and functionality
    - Added Toolbar to BottomNavigationBarActivity
    - Introduced MaterialCardView for BottomNavigationView with customized styles
    - Updated navigation graph to use new fragment IDs
    - Modified shape drawable to have rounded corners with radius of 12dp and adjusted padding
    - Added selector drawable for BottomNavigationView item colors
    - Created menu for BottomNavigationView with three items (article, dashboard, profile)
    - Enhanced BottomNavigationBarActivity class:
      - Implemented setup for Toolbar, NavController, AppBarConfiguration, and BottomNavigationView
      - Utilized androidx.navigation.ui for better navigation handling

--- app/src/main/java/com/jimbonlemu/clefer/views/BottomNavigationBarActivity.kt
+++ app/src/main/java/com/jimbonlemu/clefer/views/BottomNavigationBarActivity.kt
@@ -2,15 +2,35 @@ package com.jimbonlemu.clefer.views
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.jimbonlemu.clefer.R
 import com.jimbonlemu.clefer.core.CoreActivity
 import com.jimbonlemu.clefer.databinding.ActivityBottomNavigationBarBinding
 
 class BottomNavigationBarActivity : CoreActivity<ActivityBottomNavigationBarBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.apply {
+            setupBottomNavigationBar()
+        }
     }
 
     override fun setupBinding(layoutInflater: LayoutInflater): ActivityBottomNavigationBarBinding =
         ActivityBottomNavigationBarBinding.inflate(layoutInflater)
 
+    private fun ActivityBottomNavigationBarBinding.setupBottomNavigationBar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.hide()
+        val navController =
+            findNavController(R.id.navigation_host_fragment_activity_bottom_navigation_bar)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_article, R.id.navigation_dashboard, R.id.navigation_profile)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
 }

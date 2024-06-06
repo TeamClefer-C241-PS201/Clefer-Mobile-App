946226f80d98961bc358bffa899fc7564ced1666
astikasari123 <iqbaltech29@gmail.com>
2024-06-07 04:33:57 +0700

    feat(preview): add image preview screen
    Added a new screen for previewing images and starting analysis. Updated styles and string resources to support the new screen.
    
    - Added `PreviewImageActivity` and corresponding layout.
    - Updated `styles.xml` to include new toolbar text appearance style.
    - Updated `strings.xml` with new strings for image preview screen and Menu.

---
+++ app/src/main/java/com/jimbonlemu/clefer/views/PreviewImageActivity.kt
@@ -0,0 +1,31 @@
+package com.jimbonlemu.clefer.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.jimbonlemu.clefer.R
+import com.jimbonlemu.clefer.core.CoreActivity
+import com.jimbonlemu.clefer.databinding.ActivityPreviewImageBinding
+
+class PreviewImageActivity : CoreActivity<ActivityPreviewImageBinding>() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.apply {
+            setContentView(root)
+            setupToolbar()
+        }
+
+    }
+
+    override fun setupBinding(layoutInflater: LayoutInflater): ActivityPreviewImageBinding =
+        ActivityPreviewImageBinding.inflate(layoutInflater)
+
+    private fun ActivityPreviewImageBinding.setupToolbar() {
+        setSupportActionBar(mToolbar);
+        mToolbar.apply {
+            setNavigationIcon(R.drawable.ic_arrow_back);
+            setNavigationOnClickListener {
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
+    }
+}

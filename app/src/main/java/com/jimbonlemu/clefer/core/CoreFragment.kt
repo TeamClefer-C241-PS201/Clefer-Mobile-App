b33a3471abef5bf48e8d4972f379784805bb5d2e
astikasari123 <iqbaltech29@gmail.com>
2024-06-09 00:49:40 +0700

    feat(core): add CoreFragment base class for fragments with view binding
    Added a new abstract CoreFragment class to handle fragment view binding setup and cleanup. This helps to reduce boilerplate code in fragments that use view binding.
    
    - Created CoreFragment.kt to provide a base class for fragments with view binding.

---
+++ app/src/main/java/com/jimbonlemu/clefer/core/CoreFragment.kt
@@ -0,0 +1,34 @@
+package com.jimbonlemu.clefer.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract  class CoreFragment<VB : ViewBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = setupFragmentBinding(inflater, container, savedInstanceState)
+        return _binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    abstract fun setupFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): VB
+}

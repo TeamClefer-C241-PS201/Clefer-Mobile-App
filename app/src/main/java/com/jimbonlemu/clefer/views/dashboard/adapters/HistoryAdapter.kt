eeb4041e8e692ed9dfff2115e874c48c3c9c994c
jimbonlemu <iqbaltech29@gmail.com>
2024-06-19 11:17:31 +0700

    refactor: update history adapter and layout for improved data handling and UI
    - Refactored HistoryAdapter to use ListAdapter with DiffUtil for better list management.
    - Added a new layout for empty state representation in history data.
    - Added new string variables

--- app/src/main/java/com/jimbonlemu/clefer/views/dashboard/adapters/HistoryAdapter.kt
+++ app/src/main/java/com/jimbonlemu/clefer/views/dashboard/adapters/HistoryAdapter.kt
@@ -1,24 +1,20 @@
 package com.jimbonlemu.clefer.views.dashboard.adapters
 
-import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.net.toUri
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.jimbonlemu.clefer.databinding.ItemHistoryBinding
 import com.jimbonlemu.clefer.source.local.entity.HistoryAnalyzed
 import com.jimbonlemu.clefer.views.dashboard.AnalyzeActivity
 
-
 class HistoryAdapter(
-    private var listHistoryAnalyzed: List<HistoryAnalyzed>,
-    private val context: Context,
-    private val actionListener: HistoryActionListener,
-) : RecyclerView.Adapter<HistoryAdapter.ItemHistoryViewHolder>() {
-    class ItemHistoryViewHolder(val binding: ItemHistoryBinding) :
-        RecyclerView.ViewHolder(binding.root)
+    private val listener: HistoryActionListener,
+) : ListAdapter<HistoryAnalyzed, HistoryAdapter.ItemHistoryViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHistoryViewHolder {
         return ItemHistoryViewHolder(
@@ -26,31 +22,57 @@ class HistoryAdapter(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
-            )
+            ),
+            listener
         )
     }
 
-    override fun getItemCount(): Int = listHistoryAnalyzed.size
-
     override fun onBindViewHolder(holder: ItemHistoryViewHolder, position: Int) {
-        listHistoryAnalyzed.reversed()[position].apply {
-            with(holder.binding) {
-                Glide.with(root).load(image.toUri()).into(ivHistory)
-                tvDiseaseResult.text = diseaseResult
-                tvDateScanned.text = date
-                root.setOnClickListener {
-                    it.context.startActivity(Intent(context, AnalyzeActivity::class.java).apply {
-                        putExtra(AnalyzeActivity.PASSED_IMAGE, image)
-                        putExtra(AnalyzeActivity.PASSED_RESULT, diseaseResult)
-                        putExtra(AnalyzeActivity.PASSED_DESC, diseaseDesc)
-                        putExtra(AnalyzeActivity.PASSED_SUGGESTION, diseaseSuggestion)
-                        putExtra(AnalyzeActivity.IS_VIEWING_HISTORY, true)
-                    })
-                }
-                btnDeleteHistory.setOnClickListener {
-                    actionListener.onDeleteClicked(this@apply)
+        holder.bind(getItem(position))
+    }
+
+    class ItemHistoryViewHolder(
+        private val binding: ItemHistoryBinding,
+        private val listener: HistoryActionListener,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(historyAnalyzed: HistoryAnalyzed) {
+            historyAnalyzed.apply {
+                with(binding) {
+                    Glide.with(root).load(image.toUri()).into(ivHistory)
+                    tvDiseaseResult.text = diseaseResult
+                    tvDateScanned.text = date
+                    root.setOnClickListener {
+                        it.context.startActivity(
+                            Intent(
+                                it.context,
+                                AnalyzeActivity::class.java
+                            ).apply {
+                                putExtra(AnalyzeActivity.PASSED_IMAGE, image)
+                                putExtra(AnalyzeActivity.PASSED_RESULT, diseaseResult)
+                                putExtra(AnalyzeActivity.PASSED_DESC, diseaseDesc)
+                                putExtra(AnalyzeActivity.PASSED_SUGGESTION, diseaseSuggestion)
+                                putExtra(AnalyzeActivity.IS_VIEWING_HISTORY, true)
+                            })
+                    }
+                    btnDeleteHistory.setOnClickListener {
+                        listener.onDeleteClicked(historyAnalyzed)
+                    }
                 }
             }
+        }
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<HistoryAnalyzed>() {
+            override fun areItemsTheSame(
+                oldItem: HistoryAnalyzed,
+                newItem: HistoryAnalyzed,
+            ): Boolean = oldItem.id == newItem.id
+
+            override fun areContentsTheSame(
+                oldItem: HistoryAnalyzed,
+                newItem: HistoryAnalyzed,
+            ): Boolean = oldItem == newItem
         }
     }
 
